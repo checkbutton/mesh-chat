4af7b060c8ba80b32e5998a6ba18c9adcf687c49
Stefan Rothlehner <stefanrothlehner@Stefans-MacBook-Pro.local>
2014-03-23 14:26:36 +0100

    Initial commit

---
+++ mesh-chat/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  mesh-chat
+//
+//  Created by Stefan Rothlehner on 23.03.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
